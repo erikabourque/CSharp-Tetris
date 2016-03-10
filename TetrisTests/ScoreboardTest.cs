ec6b5361401e42dd24e19004d4b82ad898937e59
erikabourque <erika.bourque94@gmail.com>
2016-03-09 22:24:36 -0500

    moar testing

--- TetrisTests/ScoreboardTest.cs
+++ TetrisTests/ScoreboardTest.cs
@@ -70,111 +70,16 @@ namespace TetrisTests
         }
 
         [TestMethod]
-        public void incrementLinesCleared_validNum_ScoreNoBonus()
+        public void incrementLinesCleared_validNum()
         {
             // Arrange
             int numLines = 2;
-            TestBoard board = new TestBoard();
-            Scoreboard test = new Scoreboard(board);
+            IBoard board = new TestBoard();
 
             // Act
-            board.OnLinesCleared(numLines);
-            int score = test.Score;
 
             // Assert
-            Assert.AreEqual(200, score);
-        }
-
-        [TestMethod]
-        public void incrementLinesCleared_validNum_ScoreBonus()
-        {
-            // Arrange
-            int numLines = 4;
-            TestBoard board = new TestBoard();
-            Scoreboard test = new Scoreboard(board);
-
-            // Act
-            board.OnLinesCleared(numLines);
-            int score = test.Score;
-
-            // Assert
-            Assert.AreEqual(800, score);
-        }
-
-        [TestMethod]
-        public void incrementLinesCleared_validNum_Lines()
-        {
-            // Arrange
-            int numLines = 2;
-            TestBoard board = new TestBoard();
-            Scoreboard test = new Scoreboard(board);
-
-            // Act
-            board.OnLinesCleared(numLines);
-            int lines = test.Lines;
-
-            // Assert
-            Assert.AreEqual(numLines, lines);
-        }
-
-        [TestMethod]
-        public void incrementLinesCleared_validNum_Level()
-        {
-            // Arrange
-            int numLines = 2;
-            TestBoard board = new TestBoard();
-            Scoreboard test = new Scoreboard(board);
-
-            // Act
-            board.OnLinesCleared(numLines);
-            int level = test.Level;
-
-            // Assert
-            Assert.AreEqual(0, level);
-        }
-
-        [TestMethod]
-        public void incrementLinesCleared_validNum_LevelIncrease()
-        {
-            // Arrange
-            int numLines = 4;
-            TestBoard board = new TestBoard();
-            Scoreboard test = new Scoreboard(board);
-
-            // Act
-            board.OnLinesCleared(numLines);
-            board.OnLinesCleared(numLines);
-            board.OnLinesCleared(numLines);
-            int level = test.Level;
-
-            // Assert
-            Assert.AreEqual(1, level);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void incrementLinesCleared_LowerInvalidNum()
-        {
-            // Arrange
-            int numLines = 0;
-            TestBoard board = new TestBoard();
-            Scoreboard test = new Scoreboard(board);
-
-            // Act
-            board.OnLinesCleared(numLines);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void incrementLinesCleared_HigherInvalidNum()
-        {
-            // Arrange
-            int numLines = 5;
-            TestBoard board = new TestBoard();
-            Scoreboard test = new Scoreboard(board);
-
-            // Act
-            board.OnLinesCleared(numLines);
+            Assert.AreEqual();
         }
     }
 }
