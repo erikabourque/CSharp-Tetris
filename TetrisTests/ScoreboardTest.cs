5ef1e8575d2cf249f68c8d37c89263547cd81417
erikabourque <erika.bourque94@gmail.com>
2016-03-09 21:10:58 -0500

    testing yay

--- TetrisTests/ScoreboardTest.cs
+++ TetrisTests/ScoreboardTest.cs
@@ -30,6 +30,14 @@ namespace TetrisTests
         {
             throw new NotImplementedException();
         }
+
+        public void OnLinesCleared(int numLines)
+        {
+            if (LinesCleared != null)
+            {
+                LinesCleared(numLines);
+            }
+        }
     }
 
     [TestClass]
@@ -47,6 +55,31 @@ namespace TetrisTests
 
             // Assert
             Assert.IsInstanceOfType(test, typeof(Scoreboard));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void Constructor_Null()
+        {
+            // Arrange
+            Scoreboard test;
+            IBoard board = null;
+
+            // Act
+            test = new Scoreboard(board);
+        }
+
+        [TestMethod]
+        public void incrementLinesCleared_validNum()
+        {
+            // Arrange
+            int numLines = 2;
+            IBoard board = new TestBoard();
+
+            // Act
+
+            // Assert
+            Assert.AreEqual()
         }
     }
 }
